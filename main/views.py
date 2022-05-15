1b839088bfc1e13e37e032bde2dfc49facd1293c
helloworldrussia <coolgrifers@gmail.com>
2022-05-15 08:54:06 +0300

    answer on question page, some changes in answer_view

--- main/views.py
+++ main/views.py
@@ -1,28 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.core.exceptions import ObjectDoesNotExist
 from main.models import Question
 
 
+# отображение главной таблицы с категориями и ценой вопросов
 def table_view(request):
     return render(request, 'table.html')
 
 
-# cat_numb номер категории, price цена в баллах, mode 1 - выводить инпут, 0 - не выводить инпут
+# view страницы вопроса, на ней пользователь может ответить на вопрос и проверить результат
+# cat_numb номер категории, price цена в баллах, mode 1 - выводить инпут для ввода ответа на впорос
 def answer_view(request, cat_numb, price, mode):
     context = {}
     # цепляем вопрос по полученным параметрам
-    q = Question.objects.get(cat_numb=cat_numb, price=price)
-    if q == []:
+    try:
+        q = Question.objects.get(cat_numb=cat_numb, price=price)
+    except ObjectDoesNotExist:
         return render(request, 'answer.html', {'mode': False, "message": "Вопрос не найден"})
-    context['q_text'] = q.question
+
+    context['q'] = q
+    # При методе пост сравниваем полученный ответ с правильным и расширяем контекст
     if request.method == "POST":
         if request.POST['answer'] == q.answer:
-            context['message'], context['success_message'] = q.success_mess, 1
+            context['success_message'] = 1
         else:
-            context['message'], context['fail_message'] = q.fail_mess, 1
+            context['fail_message'] = 1
+
     if request.method == "GET":
         # Если mode = 1, то пользователь еще не ответил, показываем ему инпут
         if mode == 1:
             context['mode'] = True
+
     return render(request, 'answer.html', context)
