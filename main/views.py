45c98607653038bc416158674ad70d78583b4787
helloworldrussia <coolgrifers@gmail.com>
2022-05-26 13:25:42 +0300

    q_dict счетчик вопросов

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from main.models import Question
 
 
 def start_view(request):
+    request.session['score'] = 0
     context = {"background": "start.jpg", "score_off": 1}
     if request.method == "GET":
         context['first_text'] = 1
@@ -15,23 +16,39 @@ def start_view(request):
 
 def clear_score(request):
     request.session['score'] = 0
+    request.session['q_dict'] = {}
     return redirect('table')
 
 
 # отображение главной таблицы с категориями и ценой вопросов
 def table_view(request):
+    try:
+        q_dict = request.session['q_dict']
+        if len(q_dict) == 30:
+            return render(request, 'final.html', {"background": "fon.png", "score": request.session['score']})
+    except:
+        pass
     return render(request, 'table.html', {"background": "fon.png"})
 
 
 # view страницы вопроса, на ней пользователь может ответить на вопрос и проверить результат
 # cat_numb номер категории, price цена в баллах, mode 1 - выводить инпут для ввода ответа на впорос
 def answer_view(request, cat_numb, price, mode):
+
     context = {}
     # цепляем вопрос по полученным параметрам
     try:
         q = Question.objects.get(cat_numb=cat_numb, price=price)
     except ObjectDoesNotExist:
         return render(request, 'answer.html', {'mode': False, "message": "Вопрос не найден"})
+
+    try:
+        question_code = f'{cat_numb}|{price}'
+        a = request.session['q_dict'][question_code]
+        return render(request, 'answer.html', {"background": f'{q.price}.png',
+                                               'mode': False, "message": "Вы уже отвечали на этот вопрос"})
+    except:
+        pass
 
     context['q'] = q
     # При методе пост сравниваем полученный ответ с правильным и расширяем контекст
@@ -48,6 +65,12 @@ def answer_view(request, cat_numb, price, mode):
                 request.session['score'] -= q.price
             except:
                 request.session['score'] = 0 - q.price
+
+
+        try:
+            request.session['q_dict'][question_code] = 1
+        except:
+            request.session['q_dict'] = {question_code: 1}
 
     if request.method == "GET":
         # Если mode = 1, то пользователь еще не ответил, показываем ему инпут
